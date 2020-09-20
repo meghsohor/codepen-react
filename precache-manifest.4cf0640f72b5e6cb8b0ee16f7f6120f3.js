4533bbd79d5b4b7347d3607e51c51db81e6c634b
Shuvo <meghsohor@yahoo.com>
2020-09-20 20:15:13 +0600

    Updates

---
+++ precache-manifest.4cf0640f72b5e6cb8b0ee16f7f6120f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df22d22ca24a27bf5b26df1ad5db224a",
+    "url": "/codepen-react/index.html"
+  },
+  {
+    "revision": "ea2b63db9c3e27c9fea4",
+    "url": "/codepen-react/static/css/2.e980ac4b.chunk.css"
+  },
+  {
+    "revision": "b446236f6b9e7765e71c",
+    "url": "/codepen-react/static/css/main.c5db9cf5.chunk.css"
+  },
+  {
+    "revision": "ea2b63db9c3e27c9fea4",
+    "url": "/codepen-react/static/js/2.50aceed2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codepen-react/static/js/2.50aceed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b446236f6b9e7765e71c",
+    "url": "/codepen-react/static/js/main.ca70a0ae.chunk.js"
+  },
+  {
+    "revision": "9cd02c7ec32c5bc78027",
+    "url": "/codepen-react/static/js/runtime-main.97eaacb3.js"
+  }
+]);
